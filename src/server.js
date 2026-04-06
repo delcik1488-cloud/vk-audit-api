e5865569d5375e6f75f8fbf6af3b58419e56ad9c
Dima <delcik1488@gmail.com>
2026-04-06 23:23:38 +0300

    fix server listen

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
